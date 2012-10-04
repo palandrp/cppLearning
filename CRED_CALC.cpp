45922973bbd01acc14f8fae86b593f21921c6b07
Pavel Petrikovskiy <palandrp@gmail.com>
2012-10-04 17:24:57 +0600

    tab to spaces

--- CRED_CALC.cpp
+++ CRED_CALC.cpp
@@ -1,100 +1,100 @@
 /*
-	TODO:
-	Функция расчета нелинейной зависимости суммы процентов от остатка.
+    TODO:
+    Функция расчета нелинейной зависимости суммы процентов от остатка.
 */
 
 #include <iostream>
 
 using namespace std;
 
-void f_input(int 	 *sum_cred, 
-	    	 float 	 *annual_interest, 
-	    	 int 	 *number_of_monts)
+void f_input(int      *sum_cred, 
+             float      *annual_interest, 
+             int      *number_of_monts)
 {
-	cout << "1) Enter sum of credit: ";
-	cin  >> *sum_cred;
-	cout << endl;
-	cout << "2) Enter annual interest: ";
-	cin  >> *annual_interest;
-	cout << endl;
-	cout << "3) Enter number of monts: ";
-	cin  >> *number_of_monts;
-	cout << endl;
+    cout << "1) Enter sum of credit: ";
+    cin  >> *sum_cred;
+    cout << endl;
+    cout << "2) Enter annual interest: ";
+    cin  >> *annual_interest;
+    cout << endl;
+    cout << "3) Enter number of monts: ";
+    cin  >> *number_of_monts;
+    cout << endl;
 }
 
-void f_interest_charges_once(int 	sum_cred, 
-			  				 float 	annual_interest, 
-			  				 int 	number_of_monts)
+void f_interest_charges_once(int     sum_cred, 
+                               float     annual_interest, 
+                               int     number_of_monts)
 {
-	float overpayment;
-	float payment;
+    float overpayment;
+    float payment;
 
-	cout << "IF INTEREST IS CHARGED ONCE FOR THE ENTIRE AMOUNT." << endl;
+    cout << "IF INTEREST IS CHARGED ONCE FOR THE ENTIRE AMOUNT." << endl;
 
-	cout << "COUNTING: Overpayment: ";
-	overpayment = sum_cred * annual_interest * 0.01 * number_of_monts / 12;
-	cout << overpayment;
-	cout << endl;
+    cout << "COUNTING: Overpayment: ";
+    overpayment = sum_cred * annual_interest * 0.01 * number_of_monts / 12;
+    cout << overpayment;
+    cout << endl;
 
-	cout << "COUNTING: Sum of all payment for all period: ";
-	payment = sum_cred + overpayment;
-	cout << payment;
-	cout << endl;
+    cout << "COUNTING: Sum of all payment for all period: ";
+    payment = sum_cred + overpayment;
+    cout << payment;
+    cout << endl;
 
-	cout << "COUNTING: Monthly payment: ";
-	payment = (sum_cred + overpayment) / number_of_monts;
-	cout << payment;
-	cout << endl;
+    cout << "COUNTING: Monthly payment: ";
+    payment = (sum_cred + overpayment) / number_of_monts;
+    cout << payment;
+    cout << endl;
 }
 
 float f_loan_balance_math(int   sum_cred, 
-		        		  float annual_interest, 
-		        		  int   number_of_monts,
-		        		  float p)
+                          float annual_interest, 
+                          int   number_of_monts,
+                          float p)
 {
-	return (2*sum_cred - p - 12*(sum_cred + annual_interest)
-				/number_of_monts/annual_interest);
+    return (2*sum_cred - p - 12*(sum_cred + annual_interest)
+                /number_of_monts/annual_interest);
 }
 
-void f_loan_balance(int 	sum_cred, 
-		  			float 	annual_interest, 
-		  			int   	number_of_monts)
+void f_loan_balance(int     sum_cred, 
+                      float     annual_interest, 
+                      int       number_of_monts)
 {
-	cout << "IF INTEREST IS CHARGED ON THE LOAN BALANCE." << endl;
+    cout << "IF INTEREST IS CHARGED ON THE LOAN BALANCE." << endl;
 
-	for(int i = 100; i >= 0; i--)
-	{
-		float p = 0.0;
+    for(int i = 100; i >= 0; i--)
+    {
+        float p = 0.0;
 
-		cout << f_loan_balance_math(sum_cred, 
-					 				annual_interest, 
-					 				number_of_monts,
-					 				p) 
-		     << endl;
+        cout << f_loan_balance_math(sum_cred, 
+                                     annual_interest, 
+                                     number_of_monts,
+                                     p) 
+             << endl;
 
-		p += f_loan_balance_math(sum_cred, 
-				       			 annual_interest, 
-				        		 number_of_monts,
-				       			 p);
-	}
+        p += f_loan_balance_math(sum_cred, 
+                                    annual_interest, 
+                                 number_of_monts,
+                                    p);
+    }
 
 }
 
 int main(void)
 {
-	int   sum_cred = 1;
-	float annual_interest = 1.0;
-	int   number_of_monts = 1;
+    int   sum_cred = 1;
+    float annual_interest = 1.0;
+    int   number_of_monts = 1;
 
-	f_input(&sum_cred, 
-			&annual_interest, 
-			&number_of_monts);
-	
-	f_interest_charges_once(sum_cred, 
-							annual_interest, 
-							number_of_monts);
-	
-	f_loan_balance(sum_cred, 
-				   annual_interest, 
-				   number_of_monts);
+    f_input(&sum_cred, 
+            &annual_interest, 
+            &number_of_monts);
+    
+    f_interest_charges_once(sum_cred, 
+                            annual_interest, 
+                            number_of_monts);
+    
+    f_loan_balance(sum_cred, 
+                   annual_interest, 
+                   number_of_monts);
 }
--- WORK_WITH_STR.cpp
+++ WORK_WITH_STR.cpp
@@ -1,8 +1,8 @@
 /*
-	TODO:
-	Написать перегружаемые функции для реплейса символа строкой.
-	Написать перегружаемые функции для реплейса строки символом.
-	Написать перегружаемые функции для реплейса строки строкой.
+    TODO:
+    Написать перегружаемые функции для реплейса символа строкой.
+    Написать перегружаемые функции для реплейса строки символом.
+    Написать перегружаемые функции для реплейса строки строкой.
 */
 
 #include <iostream>
@@ -11,222 +11,222 @@ using namespace std;
 
 int str_len(char str[])
 {
-	int symbol = 0;
+    int symbol = 0;
 
-	for(int i = 0; str[i] != '\0'; i++)
-	{
-		symbol = i + 1;
-	}
+    for(int i = 0; str[i] != '\0'; i++)
+    {
+        symbol = i + 1;
+    }
 
-	return symbol;
+    return symbol;
 }
 
 int num_let(char str[])
 {
-	int letters = 0;
+    int letters = 0;
 
-	for(int i = 0; str[i] != '\0'; i++)
-	{
-		switch(str[i])
-		{
-			case '.'   : break;
-			case ','   : break;
-			case ' '   : break;
-			case '\''  : break;
-			case '0'   : break;
-			case '1'   : break;
-			case '2'   : break;
-			case '3'   : break;
-			case '4'   : break;
-			case '5'   : break;
-			case '6'   : break;
-			case '7'   : break;
-			case '8'   : break;
-			case '9'   : break;
-			default    : 
-				letters++;
-		}
-	}
+    for(int i = 0; str[i] != '\0'; i++)
+    {
+        switch(str[i])
+        {
+            case '.'   : break;
+            case ','   : break;
+            case ' '   : break;
+            case '\''  : break;
+            case '0'   : break;
+            case '1'   : break;
+            case '2'   : break;
+            case '3'   : break;
+            case '4'   : break;
+            case '5'   : break;
+            case '6'   : break;
+            case '7'   : break;
+            case '8'   : break;
+            case '9'   : break;
+            default    : 
+                letters++;
+        }
+    }
 
-	return letters;
+    return letters;
 }
 
 void output(char str_1[])
 {
-	cout << "Book: "   << str_1 << endl;
+    cout << "Book: "   << str_1 << endl;
 }
 
 void output(char str_1[], 
-			char str_2[])
+            char str_2[])
 {
-	cout << "Book: "   << str_1 << endl;
-	cout << "Lesson: " << str_2 << endl;
+    cout << "Book: "   << str_1 << endl;
+    cout << "Lesson: " << str_2 << endl;
 }
 
 void output(char str_1[], 
-			char str_2[], 
-			char str_3[])
+            char str_2[], 
+            char str_3[])
 {
-	cout << "Book: "    << str_1 << endl;
-	cout << "Lesson: "  << str_2 << endl;
-	cout << "Section: " << str_3 << endl;
+    cout << "Book: "    << str_1 << endl;
+    cout << "Lesson: "  << str_2 << endl;
+    cout << "Section: " << str_3 << endl;
 }
 
 void f_repl_input(char *letter, 
-				  char *repl_letter)
+                  char *repl_letter)
 {
-	cout << "Replace letter: ";
-	cin  >> *letter;
-	cout << "on: ";
-	cin  >> *repl_letter;
+    cout << "Replace letter: ";
+    cin  >> *letter;
+    cout << "on: ";
+    cin  >> *repl_letter;
 }
 
 void f_repl_letters(char letter, 
-				    char repl_letter, 
-				    char str_1[])
+                    char repl_letter, 
+                    char str_1[])
 {
-	int i = 0;
+    int i = 0;
 
-	while(str_1[i] != '\0')
-	{
-		if (str_1[i]  == letter)
-			str_1[i]  =  repl_letter;
-		i++;
-	}
+    while(str_1[i] != '\0')
+    {
+        if (str_1[i]  == letter)
+            str_1[i]  =  repl_letter;
+        i++;
+    }
 }
 
 void f_repl_letters(char letter, 
-				    char repl_letter, 
-				    char str_1[], 
-				    char str_2[])
+                    char repl_letter, 
+                    char str_1[], 
+                    char str_2[])
 {
-	int i = 0;
+    int i = 0;
 
-	while(str_1[i] != '\0')
-	{
-		if (str_1[i]  == letter)
-			str_1[i]  =  repl_letter;
-		i++;
-	}
+    while(str_1[i] != '\0')
+    {
+        if (str_1[i]  == letter)
+            str_1[i]  =  repl_letter;
+        i++;
+    }
 
-	i = 0;
+    i = 0;
 
-	while(str_2[i] != '\0')
-	{
-		if (str_2[i]  == letter)
-			str_2[i]  =  repl_letter;
-		i++;
-	}
+    while(str_2[i] != '\0')
+    {
+        if (str_2[i]  == letter)
+            str_2[i]  =  repl_letter;
+        i++;
+    }
 }
 
 void f_repl_letters(char letter, 
-				    char repl_letter, 
-				    char str_1[], 
-				    char str_2[], 
-				    char str_3[])
+                    char repl_letter, 
+                    char str_1[], 
+                    char str_2[], 
+                    char str_3[])
 {
-	int i = 0;
+    int i = 0;
 
-	while(str_1[i] != '\0')
-	{
-		if (str_1[i]  == letter)
-			str_1[i]  =  repl_letter;
-		i++;
-	}
+    while(str_1[i] != '\0')
+    {
+        if (str_1[i]  == letter)
+            str_1[i]  =  repl_letter;
+        i++;
+    }
 
-	i = 0;
+    i = 0;
 
-	while(str_2[i] != '\0')
-	{
-		if (str_2[i]  == letter)
-			str_2[i]  =  repl_letter;
-		i++;
-	}
+    while(str_2[i] != '\0')
+    {
+        if (str_2[i]  == letter)
+            str_2[i]  =  repl_letter;
+        i++;
+    }
 
-	i = 0;
+    i = 0;
 
-	while(str_3[i] != '\0')
-	{
-		if (str_3[i]  == letter)
-			str_3[i]  =  repl_letter;
-		i++;
-	}
+    while(str_3[i] != '\0')
+    {
+        if (str_3[i]  == letter)
+            str_3[i]  =  repl_letter;
+        i++;
+    }
 }
 
 void f_replace(char str_1[])
 {
-	char letter 	 = '\0';
-	char repl_letter = '\0';
+    char letter      = '\0';
+    char repl_letter = '\0';
 
-	f_repl_input(&letter, 
-				 &repl_letter);
+    f_repl_input(&letter, 
+                 &repl_letter);
 
-	f_repl_letters(letter, 
-				   repl_letter, 
-				   str_1);
+    f_repl_letters(letter, 
+                   repl_letter, 
+                   str_1);
 
-	output(str_1);
+    output(str_1);
 }
 
 void f_replace(char str_1[], 
-			   char str_2[])
+               char str_2[])
 {
-	char letter 	 = '\0';
-	char repl_letter = '\0';
+    char letter      = '\0';
+    char repl_letter = '\0';
 
-	f_repl_input(&letter, 
-				 &repl_letter);
+    f_repl_input(&letter, 
+                 &repl_letter);
 
-	f_repl_letters(letter, 
-				   repl_letter, 
-				   str_1, 
-				   str_2);
+    f_repl_letters(letter, 
+                   repl_letter, 
+                   str_1, 
+                   str_2);
 
-	output(str_1, 
-		   str_2);
+    output(str_1, 
+           str_2);
 }
 
 void f_replace(char str_1[], 
-			   char str_2[],
-			   char str_3[])
+               char str_2[],
+               char str_3[])
 {
-	char letter 	 = '\0';
-	char repl_letter = '\0';
+    char letter      = '\0';
+    char repl_letter = '\0';
 
-	f_repl_input(&letter, 
-				 &repl_letter);
+    f_repl_input(&letter, 
+                 &repl_letter);
 
-	f_repl_letters(letter, 
-				   repl_letter, 
-				   str_1, 
-				   str_2, 
-				   str_3);
+    f_repl_letters(letter, 
+                   repl_letter, 
+                   str_1, 
+                   str_2, 
+                   str_3);
 
-	output(str_1, 
-		   str_2, 
-		   str_3);
+    output(str_1, 
+           str_2, 
+           str_3);
 }
 
 int main(void)
 {
-	char title[]   = "High mathematica. B2.";
-	char lesson[]  = "Pascal's algorithms.";
-	char section[] = "Pascal's method.";
+    char title[]   = "High mathematica. B2.";
+    char lesson[]  = "Pascal's algorithms.";
+    char section[] = "Pascal's method.";
 
-	output(title, 
-		   lesson);
+    output(title, 
+           lesson);
 
-	cout << "Length of first string is "  
-		 << str_len(title)  << endl;
-	cout << "Length of second string is " 
-		 << str_len(lesson) << endl;
+    cout << "Length of first string is "  
+         << str_len(title)  << endl;
+    cout << "Length of second string is " 
+         << str_len(lesson) << endl;
 
-	cout << "Number of letters in first string is "  
-		 << num_let(title) 	<< endl;
-	cout << "Number of letters in second string is " 
-		 << num_let(lesson) << endl;
+    cout << "Number of letters in first string is "  
+         << num_let(title)     << endl;
+    cout << "Number of letters in second string is " 
+         << num_let(lesson) << endl;
 
-	f_replace(title, 
-			  lesson, 
-			  section);
+    f_replace(title, 
+              lesson, 
+              section);
 }
